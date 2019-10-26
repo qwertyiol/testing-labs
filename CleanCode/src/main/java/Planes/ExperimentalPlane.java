030518e1a4f81a6057f028aa9485165e0dd4328b
qwertyiol <47792442+qwertyiol@users.noreply.github.com>
2019-10-26 09:23:50 +0300

    fixed files

---
+++ CleanCode/src/main/java/Planes/ExperimentalPlane.java
@@ -0,0 +1,50 @@
+package Planes;
+
+import models.ClassificationLevel;
+import models.ExperimentalTypes;
+
+public class ExperimentalPlane extends Plane{
+
+    private ExperimentalTypes type;
+    private ClassificationLevel classificationLevel;
+
+    public ExperimentalPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, ExperimentalTypes type, ClassificationLevel classificationLevel) {
+        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
+        this.type = type;
+        this.classificationLevel = classificationLevel;
+    }
+
+    public ExperimentalTypes getType() {
+        return type;
+    }
+
+    public ClassificationLevel getClassificationLevel(){
+        return classificationLevel;
+    }
+
+    public void setType(ExperimentalTypes type) {
+        this.type = type;
+    }
+
+    public void setClassificationLevel(ClassificationLevel classificationLevel){
+        this.classificationLevel = classificationLevel;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        return super.equals(object);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "ExperimentalPlane{" +
+                "type=" + type.name() +
+                ";classificationLevel='" + classificationLevel.name() + ';' +
+                '}';
+    }
+}
